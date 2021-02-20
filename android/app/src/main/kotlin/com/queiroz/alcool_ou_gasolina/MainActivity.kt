f87bef6cd9dc03d5eee18d9a21b187862d84a5f4
jhonathanqz029 <74057391+jhonathanqz029@users.noreply.github.com>
2021-02-20 14:22:11 -0300

    Alcool_ou_Gasolina

---
+++ android/app/src/main/kotlin/com/queiroz/alcool_ou_gasolina/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.queiroz.alcool_ou_gasolina
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Home.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  TextEditingController _controllerAlcool = TextEditingController();
+  TextEditingController _controllerGasolina = TextEditingController();
+  String _textoResultado = "";
+
+
+  void _calcular(){
+
+    double precoAlcool = double.tryParse( _controllerAlcool.text );
+    double precoGasolina = double.tryParse( _controllerGasolina.text );
+
+    if( precoAlcool == null || precoGasolina == null ){
+      setState(() {
+        _textoResultado = "Número inválido, digite números maiores que 0 e utilizando (.) ";
+      });
+    }else{
+
+      /*
+      * Se o preço do álcool divido pelo preço da gasolina
+      * for >= a 0.7 é melhor abastecer com gasolina
+      * senão é melhor utilizar álcool
+      * */
+      if( (precoAlcool / precoGasolina) >= 0.7 ){
+        setState(() {
+          _textoResultado = "Melhor abastecer com Gasolina!";
+        });
+      }else{
+        setState(() {
+          _textoResultado = "Melhor abastecer com Etanol!";
+        });
+      }
+
+      //_limparCampos();
+
+    }
+
+
+  }
+
+  void _limparCampos(){
+    _controllerGasolina.text = "";
+    _controllerAlcool.text = "";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange[600],
+      appBar: AppBar(
+        title: Text("Etanol ou Gasolina", style: TextStyle(
+          color: Colors.white, fontWeight: FontWeight.bold
+        ),),
+        backgroundColor: Colors.orange[800]
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 32),
+                child: Image.asset("imagens/combustivel.jpg"),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "Saiba qual a melhor opção para abastecimento do seu carro",
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Preço Etanol, ex: 1.59", 
+                    labelStyle: TextStyle(color: Colors.grey[900],fontWeight: FontWeight.bold ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(15)),
+                ),
+                style: TextStyle(
+                    fontSize: 22
+                ),
+                controller: _controllerAlcool,
+              ),
+              SizedBox(height: 20,),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Preço Gasolina, ex: 3.59",
+                    labelStyle: TextStyle(color: Colors.grey[900],fontWeight: FontWeight.bold ),
+                    focusColor: Colors.grey,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(15)),
+
+                ),
+                style: TextStyle(
+                    fontSize: 22
+                ),
+                controller: _controllerGasolina,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: RaisedButton(
+                    color: Colors.black,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.all(15),
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(
+                          fontSize: 20
+                      ),
+                    ),
+                    onPressed: _calcular
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  _textoResultado,
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
